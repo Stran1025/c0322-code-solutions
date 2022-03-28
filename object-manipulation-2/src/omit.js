3022917feb9127284e4e5eee5267741de29e4e1a
Sang Tran <sang.tran1025@gmail.com>
2022-03-28 18:47:55 +0000

    inprogress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,22 @@
 /* exported omit */
+
+/*
+create an empty object and assign it as output
+make the output the same as the input source object
+for each property in the source object
+check if the keys matches the properties in the input source
+if it is a match remove the properties from the output object
+return output object
+*/
+
+function omit(source, keys) {
+  var output = source;
+  for (var prop in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (prop === keys[i]) {
+        delete output[prop];
+      }
+    }
+  }
+  return output;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,26 @@
 /* exported pick */
+
+/*
+assign an empty object as output
+get all the properties in the source object
+check each and every property in the input object if the input keys matches
+check if the value of the matched properties is undefined
+if it is undefined dont add the keys to the outut
+if the key match then add the property and its value to the output
+repeat until no more key is left in input array
+return output
+*/
+
+function pick(source, keys) {
+  var output = {};
+  for (var prop in source) {
+    for (var i = 0; i < keys.length; i++) {
+      if (prop === keys[i]) {
+        if (source[prop] !== undefined) {
+          output[keys[i]] = source[prop];
+        }
+      }
+    }
+  }
+  return output;
+}
