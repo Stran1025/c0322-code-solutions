2a380a19f197deed35af514d16229b2bb33e5afa
Sang Tran <sang.tran1025@gmail.com>
2022-03-29 19:49:19 +0000

    change variable name

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -4,22 +4,22 @@ var $room = document.querySelector('.room');
 var $roomClassList = $room.classList;
 var $buttonContainer = document.querySelector('.button-container');
 var $buttonContainerClassList = $buttonContainer.classList;
-var status = true;
+var isLightOn = true;
 
 function lightswitch(event) {
-  if (status === 'true') {
+  if (isLightOn) {
     $lightBulb.className = 'light-off';
     $roomClassList.remove('light-on');
     $roomClassList.add('light-off');
     $buttonContainerClassList.remove('light-on');
     $buttonContainerClassList.add('light-off');
-    status = false;
+    isLightOn = false;
   } else {
     $lightBulb.className = 'light-on';
     $roomClassList.remove('light-off');
     $roomClassList.add('light-on');
     $buttonContainerClassList.remove('light-off');
     $buttonContainerClassList.add('light-on');
-    status = true;
+    isLightOn = true;
   }
 }
