edb1773829ca553ab4772c394d42d643e142312c
Sang Tran <sang.tran1025@gmail.com>
2022-03-29 18:49:13 +0000

    added variables for javascript to track the status of the bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -4,19 +4,22 @@ var $room = document.querySelector('.room');
 var $roomClassList = $room.classList;
 var $buttonContainer = document.querySelector('.button-container');
 var $buttonContainerClassList = $buttonContainer.classList;
+var status = 'light-on';
 
 function lightswitch(event) {
-  if ($lightBulb.className === 'light-on') {
+  if (status === 'light-on') {
     $lightBulb.className = 'light-off';
     $roomClassList.remove('light-on');
     $roomClassList.add('light-off');
     $buttonContainerClassList.remove('light-on');
     $buttonContainerClassList.add('light-off');
+    status = 'light-off';
   } else {
     $lightBulb.className = 'light-on';
     $roomClassList.remove('light-off');
     $roomClassList.add('light-on');
     $buttonContainerClassList.remove('light-off');
     $buttonContainerClassList.add('light-on');
+    status = 'light-on';
   }
 }
