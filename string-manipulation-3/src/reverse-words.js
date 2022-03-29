97615ec36b5def7be12648a8a2d702be2ef32430
Sang Tran <sang.tran1025@gmail.com>
2022-03-29 17:24:28 +0000

    inprogress

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,26 @@
 /* exported reverseWords */
+
+/*
+assign an empty string as output
+assign an empty string for word
+break the input string into an array separating by ' '
+for each item in the array
+  for each letter in the array index reverse the order
+  add the letter into word
+concatanate the words into a string
+return output string
+*/
+
+function reverseWords(string) {
+  var output = '';
+  var word = '';
+  var array = string.split(' ');
+  for (var i = 0; i < array.length; i++) {
+    for (var j = array[i] - 1; j >= 0; j--) {
+      word += array[i][j];
+    }
+    output = word + ' ';
+  }
+  output += word;
+  return output;
+}
