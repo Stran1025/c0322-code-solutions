e86a998204224156593d3881046c35c0873fa1db
Sang Tran <sang.tran1025@gmail.com>
2022-03-30 02:15:35 +0000

    completed javascript-typing-tutor challenge

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,13 +1,37 @@
 var $prompt = document.querySelectorAll('span.letter');
+var $popUpWindow = document.querySelector('#pop-up-window');
+var $yesButton = document.querySelector('#yes-button');
+var $accuracyDisplay = document.querySelector('#accuracy');
+var count = 0;
+var numberOfKeysInput = 0;
 
-document.addEventListener('keydown', test);
+document.addEventListener('keydown', keyPressed);
+$yesButton.addEventListener('click', playAgain);
 
-function test(event) {
-  for (var i = 0; i < $prompt.length; i++) {
-    if (event.key === $prompt[i].textContent) {
-      $prompt[i].classList.add('passed-letter');
-      $prompt[i].classList.remove('current-letter');
-      $prompt[i + 1].classList.add('current-letter');
+function keyPressed(event) {
+  numberOfKeysInput++;
+  if (event.key === $prompt[count].textContent) {
+    $prompt[count].classList.add('passed-letter');
+    $prompt[count].classList.remove('current-letter');
+    $prompt[count].classList.remove('incorrect-letter');
+    count++;
+    if (count === $prompt.length) {
+      $popUpWindow.classList.remove('hidden');
+      $accuracyDisplay.textContent = 'Accuracy: ' + (30 / numberOfKeysInput * 100) + '%';
+      numberOfKeysInput = 0;
+      return;
     }
+    $prompt[count].classList.add('current-letter');
+  } else {
+    $prompt[count].classList.add('incorrect-letter');
   }
+}
+
+function playAgain(event) {
+  count = 0;
+  for (var i = 0; i < $prompt.length; i++) {
+    $prompt[i].classList.remove('passed-letter');
+  }
+  $prompt[0].classList.add('current-letter');
+  $popUpWindow.classList.add('hidden');
 }
