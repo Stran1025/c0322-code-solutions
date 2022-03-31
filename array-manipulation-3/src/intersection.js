f312d08c04a00bfae8e12ae5ec124109d951efa7
Sang Tran <sang.tran1025@gmail.com>
2022-03-31 06:30:00 +0000

    Completed array-manipulation-3 challenge

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,21 @@
 /* exported intersection */
+
+/*
+assign an empty array as output
+for each item in the first array
+  match it agaisnt the second array
+    if the item matches add it to the output;
+return output
+*/
+
+function intersection(first, second) {
+  var output = [];
+  for (var i = 0; i < first.length; i++) {
+    for (var j = 0; j < second.length; j++) {
+      if (first[i] === second[j]) {
+        output.push(first[i]);
+      }
+    }
+  }
+  return output;
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,30 @@
 /* exported unique */
+
+/*
+assign an empty array as output
+push the first item of the array into output
+for each item in the input array
+  check if it is in the output array already
+  if the item is already in the output array do nothing
+  if the item is not anywhere in the output array
+  push the item to the output
+return output
+*/
+
+function unique(array) {
+  if (array.length === 0) {
+    return [];
+  }
+  var output = [];
+  output.push(array[0]);
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < output.length; j++) {
+      if (array[i] === output[j]) {
+        break;
+      } else if (array[i] !== output[j] && j === output.length - 1) {
+        output.push(array[i]);
+      }
+    }
+  }
+  return output;
+}
