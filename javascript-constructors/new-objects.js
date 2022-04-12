2327bd3d90630bc1002eb891452eed7dc04f1f22
Sang Tran <sang.tran1025@gmail.com>
2022-04-12 17:48:47 +0000

    Completed javascript-constructors exercise

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var anObject = new Object();
+console.log('value for new Object: ', anObject);
+console.log('typeof new Object: ', typeof anObject);
+
+var anArray = new Array();
+console.log('value for new Array: ', anArray);
+console.log('typeof new Array: ', typeof anArray);
+
+var aFunction = new Function();
+console.log('value for new function: ', aFunction);
+console.log('typeof new function: ', typeof aFunction);
+
+var aString = new String();
+console.log('value for new string: ', aString);
+console.log('typeof new string: ', typeof aString);
+
+var aNumber = new Number();
+console.log('value for new number: ', aNumber);
+console.log('typeof new number: ', typeof aNumber);
+
+var aBoolean = new Boolean();
+console.log('value for new boolean: ', aBoolean);
+console.log('typeof new boolean: ', typeof aBoolean);
