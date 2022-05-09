4089ed90ea1a20badb9f679ae03bc797962c5fa5
Sang Tran <sang.tran1025@gmail.com>
2022-05-09 22:00:01 +0000

    Clients can request notes by id

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -8,9 +8,21 @@ app.get('/api/notes', (req, res) => {
     notesArray.push(data.notes[property]);
   }
   res.send(notesArray);
+});
 
+app.get('/api/notes/:id', (req, res) => {
+  if (isNaN(parseInt(req.params.id))) {
+    const error = { error: `the id need to be a number, ${req.params.id} is not a number` };
+    res.status(400).json(error);
+    return;
+  } else if (!data.notes[req.params.id]) {
+    const error = { error: `cannot find note with id of ${req.params.id}` };
+    res.status(404).json(error);
+    return;
+  }
+  res.json(data.notes[req.params.id]);
 });
 
 app.listen(3000, () => {
-  process.stdout.write('Server is listening on port 3000!');
+  process.stdout.write('Server is listening on port 3000! \n');
 });
