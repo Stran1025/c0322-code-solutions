7c00a2674d77ad4d6d258efe991f58f7b78a0a0e
Sang Tran <sang.tran1025@gmail.com>
2022-05-18 22:17:17 +0000

    created hot-button.jsx and moved the component definition to that file

---
+++ react-hot-button-component/src/hot-button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clickCount: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    let count = this.state.clickCount;
+    count++;
+    this.setState({ clickCount: count });
+  }
+
+  render() {
+    const count = this.state.clickCount;
+    if (count >= 15) {
+      return <button onClick={this.handleClick} className='level-6'>Hot Button</button>;
+    }
+    if (count >= 12) {
+      return <button onClick={this.handleClick} className='level-5'>Hot Button</button>;
+    }
+    if (count >= 9) {
+      return <button onClick={this.handleClick} className='level-4'>Hot Button</button>;
+    }
+    if (count >= 6) {
+      return <button onClick={this.handleClick} className='level-3'>Hot Button</button>;
+    }
+    if (count >= 3) {
+      return <button onClick={this.handleClick} className='level-2'>Hot Button</button>;
+    }
+    return <button onClick={this.handleClick} className='level-1'>Hot Button</button>;
+  }
+}
+
+export default <HotButton/>;
--- react-hot-button-component/src/index.jsx
+++ react-hot-button-component/src/index.jsx
@@ -1,41 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HotButton from './hot-button.jsx';
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
-
-class HotButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { clickCount: 0 };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    let count = this.state.clickCount;
-    count++;
-    this.setState({ clickCount: count });
-  }
-
-  render() {
-    const count = this.state.clickCount;
-    if (count >= 15) {
-      return <button onClick={this.handleClick} className='level-6'>Hot Button</button>;
-    }
-    if (count >= 12) {
-      return <button onClick={this.handleClick} className='level-5'>Hot Button</button>;
-    }
-    if (count >= 9) {
-      return <button onClick={this.handleClick} className='level-4'>Hot Button</button>;
-    }
-    if (count >= 6) {
-      return <button onClick={this.handleClick} className='level-3'>Hot Button</button>;
-    }
-    if (count >= 3) {
-      return <button onClick={this.handleClick} className='level-2'>Hot Button</button>;
-    }
-    return <button onClick={this.handleClick} className='level-1'>Hot Button</button>;
-  }
-}
-
-root.render(<HotButton/>);
+root.render(HotButton);
